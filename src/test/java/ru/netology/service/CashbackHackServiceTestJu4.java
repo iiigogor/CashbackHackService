e079a80e75a12ac2c58718c8f9156907fda04e2c
unknown <iiigogor@gmail.com>
2022-02-22 14:52:25 +0300

    commit junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJu4.java
@@ -0,0 +1,26 @@
+package ru.netology.service;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CashbackHackServiceTestJu4 {
+    @Test
+    public void belowTheBorder() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        assertEquals(100, actual);
+    }
+
+    @Test
+    public void aboveTheBorder() {
+        CashbackHackService service = new CashbackHackService();
+        assertEquals(900,service.remain(1100));
+    }
+    @Ignore
+    @Test
+    public void  equalsBorder(){
+        CashbackHackService service= new CashbackHackService();
+        int amount=1000;
+        assertEquals(0,service.remain(1000));
+    }
+}
